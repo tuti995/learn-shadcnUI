cb22ac7ee3fbfa88370b2d03ee63d54f3429e38b
tuti <tsuchi995@gmail.com>
2024-03-23 23:49:12 +0900

    add: button
    npx shadcn-ui@latest add button

---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-24">
+      <Button>click</Button>
+    </main>
+  );
+}
