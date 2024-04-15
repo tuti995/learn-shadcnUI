2213f73d2264b3f2e50cd7a0ad123848ec6f9637
tuti <tsuchi995@gmail.com>
2024-04-15 22:54:21 +0900

    add: alert

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+import { Terminal } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 export default function Home() {
   return (
     <main className="p-24">
       <Button>click</Button>
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </main>
   );
 }
