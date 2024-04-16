e5604e9f4f382143fe8132333b30341b7625cb61
tuti <tsuchi995@gmail.com>
2024-04-16 22:46:35 +0900

    add: calendar

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,13 +1,16 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import * as React from "react";
 import Image from "next/image";
 
 import { Terminal } from "lucide-react";
-
+import { Button } from "@/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Calendar } from "@/components/ui/calendar";
 
 export default function Home() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
     <main className="p-24">
       <Button>click</Button>
@@ -22,6 +25,12 @@ export default function Home() {
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
     </main>
   );
 }
